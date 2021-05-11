c3290f510b926a112c9e680770514cd4798c9ecb
Lucas Daniel Arcanjo <lucasarcanjo.ld@gmail.com>
2021-05-10 21:24:55 -0300

    Adds new module: Accounts.

---
+++ src/modules/accounts/useCases/createUser/CraeteUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDto } from "../../dtos/ICreateUserDTO";
+import { UsersRepository } from "../../repositories/implementations/UsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: UsersRepository
+  ) {}
+
+  async execute({
+    name,
+    username,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDto): Promise<void> {
+    await this.usersRepository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
