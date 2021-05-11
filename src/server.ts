def9f00d7dd0412c4b63038eb004ae43a0f213cf
Lucas Daniel Arcanjo <lucasarcanjo.ld@gmail.com>
2021-05-10 22:26:53 -0300

    Adds custom errors.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import cors from "cors";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
@@ -8,6 +9,7 @@ import swaggerFile from "./swagger.json";
 
 import "./database";
 import "./shared/container";
+import { AppError } from "./errors/AppError";
 
 const app = express();
 
@@ -15,5 +17,20 @@ app.use(cors());
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${error.message}`,
+    });
+  }
+);
 
 app.listen(3333, () => console.log("Server is running!"));
