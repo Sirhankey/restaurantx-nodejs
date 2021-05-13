32207223f30aa25d694dbacb2b2971274da25eb2
Lucas Daniel Arcanjo <lucasarcanjo.ld@gmail.com>
2021-05-12 21:32:46 -0300

    Added avatar file in users.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUseAvatar/updateUserAvatarController";
 
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 const usersRoutes = Router();
 const createUserController = new CreateUserController();
+const updateUserAvatarUseController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarUseController.handle
+);
 
 export { usersRoutes };
