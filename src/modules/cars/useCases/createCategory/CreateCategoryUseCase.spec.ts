bf01fe505446e91258be2f0af1b40d09167d85ea
Lucas Daniel Arcanjo <lucasarcanjo.ld@gmail.com>
2021-05-17 20:57:27 -0300

    Adds unit tests.

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,36 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/inMemory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+describe("Create Category", () => {
+  let createCategoryUseCase: CreateCategoryUseCase;
+  let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
+  });
+
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Description Test",
+    };
+    await createCategoryUseCase.execute(category);
+    const result = await categoriesRepositoryInMemory.findByName(category.name);
+    expect(result).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test",
+        description: "Description Test",
+      };
+      await createCategoryUseCase.execute(category);
+      await createCategoryUseCase.execute(category);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
