2ef87f95c59513035337e68369585d2cae2c12d5
Lucas Daniel Arcanjo <lucasarcanjo.ld@gmail.com>
2021-05-17 21:52:32 -0300

    adds unit tests for authenticateUserUseCase.

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUseCase.spec.ts
@@ -0,0 +1,66 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDto } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/inMemory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CraeteUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+describe("Authenticate User", () => {
+  let authenticateUserUseCase: AuthenticateUserUseCase;
+  let usersRepositoryInMemory: UsersRepositoryInMemory;
+  let createUserUseCase: CreateUserUseCase;
+
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    authenticateUserUseCase = new AuthenticateUserUseCase(
+      usersRepositoryInMemory
+    );
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it("should be able to authenticate an user", async () => {
+    const user: ICreateUserDto = {
+      driver_license: "000123",
+      email: "user@test.com",
+      password: "12345",
+      name: "User Test",
+    };
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("should not be able to authenticate an nonexistent user", async () => {
+    expect(async () => {
+      const user: ICreateUserDto = {
+        driver_license: "000123",
+        email: "user@test.com",
+        password: "12345",
+        name: "User Test",
+      };
+      await createUserUseCase.execute(user);
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should not be able to authenticate with incorrect password", async () => {
+    expect(async () => {
+      const user: ICreateUserDto = {
+        driver_license: "000123",
+        email: "user@test.com",
+        password: "12345",
+        name: "User Test",
+      };
+      await createUserUseCase.execute(user);
+
+      await createUserUseCase.execute(user);
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "1234",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
